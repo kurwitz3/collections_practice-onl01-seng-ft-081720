b47d226e23c7dabede45c5a4859840fe3dd1e428
Richard Kurwitz <kurwitz3@aol.com>
2020-08-20 17:58:30 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,29 @@
+def sort_array_asc(array)
+array.sort 
+end
+def sort_array_desc(array)
+  array.sort!{|x,y| y <=> x}
+end
+ def sort_array_char_count(array)
+   array.sort {|a,b| a.length <=> b.length}
+   end
+ def swap_elements(array)
+   array[1], array[2] = array[2], array[1]
+   array
+ 
+ end
+ def reverse_array(array)
+   array.reverse
+ end
+def kesha_maker(array)
+   money = []
+  array.each do |x|
+  x[2] = "$"
+  money << x 
+  end 
+  money
+end 
+def find_a(array)
 
+end
+    
